77a8b7a0053ccbe30fca1a24a9d670ae720c9a81
Julius Oh <juliusoh@gmail.com>
2022-08-27 16:16:31 -0700

    backend s3 bucket

--- main.tf
+++ main.tf
@@ -2,6 +2,35 @@ locals {
   vpc = lookup(var.vpc, var.environment)
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-julius"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 resource "aws_vpc" "vpc" {
   cidr_block           = local.vpc.cidr_block
   enable_dns_hostnames = true
@@ -35,16 +64,16 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    tags = {
-        Environment = var.environment
-        Name        = "public-rt-${var.environment}"
-    }
+  tags = {
+    Environment = var.environment
+    Name        = "public-rt-${var.environment}"
+  }
 }
 
 resource "aws_route" "public_route" {
-    route_table_id         = aws_route_table.public_rt.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id             = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
--- providers.tf
+++ providers.tf
@@ -7,8 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  region                   = var.aws_region
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = var.aws_profile
+  region = var.aws_region
 }
 
